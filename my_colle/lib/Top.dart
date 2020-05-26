cde176ef0633b56d2dc6739b23a6564516faa3e6
Sena Sakata <sakata-sena@softcomm.co.jp>
2020-05-26 17:21:30 +0900

    Top画面デザイン修正

--- my_colle/lib/Top.dart
+++ my_colle/lib/Top.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:my_colle/Style.dart';
 
 class Top extends StatelessWidget {
-
+  Size size;
   @override
   Widget build(BuildContext context) {
+    this.size = MediaQuery.of(context).size;
+    
     return Scaffold(
       appBar: AppBar(),
       endDrawer: Drawer(),
@@ -16,51 +19,135 @@ class Top extends StatelessWidget {
               fit: BoxFit.cover,
             ),
           ),
-          Column(
+          Column(            
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
+              Style.height(this.size.height/6.8),
               Container(
                 child: Column(
                   children: <Widget>[
-                    Text('集めるだけじゃ、もったいない。'),
-                    Text('マイコレ'),
+                    Text(
+                      '集めるだけじゃ、もったいない。',
+                      style: TextStyle(
+                        color: Color(0xffFFFFFF),
+                        fontSize: 24.0,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: Style.font,
+                        locale: Locale("ja", "JP"),
+                      ),
+                    ),
+                    Text(
+                      'マイコレ',
+                      style: TextStyle(
+                        color: Color(0xffFFFFFF),
+                        fontSize: 70.0,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: Style.font,
+                        locale: Locale("ja", "JP"),
+                      ),
+                    ),
                   ],
                 ),
               ),
               Container(
                 child: Column(
                   children: <Widget>[
-                    Text('<今月の特集>'),
-                    Text('カメラ、双眼鏡 特集'),
+                    Text(
+                      '<今月の特集>',
+                      style: TextStyle(
+                        color: Color(0xffFFFFFF),
+                        fontSize: 22.0,
+                        fontFamily: Style.font,
+                        locale: Locale("ja", "JP"),
+                      ),
+                    ),
+                    Text(
+                      'カメラ、双眼鏡 特集',
+                      style: TextStyle(
+                        color: Color(0xffFFFFFF),
+                        fontSize: 22.0,
+                        fontFamily: Style.font,
+                        locale: Locale("ja", "JP"),
+                      ),
+                    ),
                   ],
                 ),
+                decoration: BoxDecoration(
+                  color: Color(0xffad0000).withOpacity(0.9),
+                ),
+                width: this.size.width - 20,
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Container(
-                    padding: EdgeInsets.all(10.0),
-                    child: RaisedButton(
-                      child: Text('みんなの\nMY ROOM'),
+              Style.height(this.size.height/3.4),
+              Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    FlatButton(
+                      child: Container(
+                        width: 180.0,
+                        child: Center(
+                          child: Text(
+                            ' みんなの\nMY ROOM',
+                            style: TextStyle(
+                              color: Color(0xffFFFFFF),
+                              fontSize: 20.0,
+                              fontFamily: Style.font,
+                              locale: Locale("ja", "JP"),
+                            ),
+                          ),
+                        ), 
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Color(0xffFFFFFF)),
+                          borderRadius: BorderRadius.circular(10),
+                          color: Color(0xffad0000).withOpacity(0.9),
+                        ),
+                        padding: EdgeInsets.all(10.0),
+                      ),
                       onPressed: () {
                         Navigator.pushNamed(context, '/ColleDtl');
-                      }
+                      },
+                      padding: EdgeInsets.all(0.0),
                     ),
-                  ),
-                  Container(
-                    padding: EdgeInsets.all(10.0),
-                    child: RaisedButton(
-                      child: Text('わたしの\nMY ROOM'),
+                    Style.width(8),
+                    FlatButton(
+                      child: Container(
+                        width: 180.0,
+                        child: Center(
+                          child: Text(
+                            ' わたしの\nMY ROOM',
+                            style: TextStyle(
+                              color: Color(0xffFFFFFF),
+                              fontSize: 20.0,
+                              fontFamily: Style.font,
+                              locale: Locale("ja", "JP"),
+                            ),
+                          ),
+                        ), 
+                        decoration: BoxDecoration(
+                          border: Border.all(color: Color(0xffFFFFFF)),
+                          borderRadius: BorderRadius.circular(10),
+                          color: Color(0xffad0000).withOpacity(0.9),
+                        ),
+                        padding: EdgeInsets.all(10.0),
+                      ),
                       onPressed: () {
-                        Navigator.pushNamed(context, '/CreMyRm');
-                      }
+                        Navigator.pushNamed(context, '/ColleDtl');
+                      },
+                      padding: EdgeInsets.all(0.0),
                     ),
-                  ),
-                ],
+                  ],
+                ),
+                margin: EdgeInsets.all(10.0),
+                padding: EdgeInsets.all(2.0),
+                width: this.size.width - 20,
+                decoration: BoxDecoration(
+                  color: Color(0xffad0000).withOpacity(0.9),
+                ),
               ),
             ],
           ),
         ],
-      )
+      ),
     );
   }
 }
