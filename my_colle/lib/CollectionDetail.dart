31a81f7a508a22420afd27e638f7d778e9ae8028
ikizakura <ikizakura-ryo@softcomm.co.jp>
2020-05-28 22:37:18 +0900

    コレクション詳細画面の画面遷移を実装しました

--- my_colle/lib/CollectionDetail.dart
+++ my_colle/lib/CollectionDetail.dart
@@ -2,8 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:my_colle/dto/Collection.dart';
 
 class CollectionDetail extends StatelessWidget {
+  BuildContext context;
+
   @override
   Widget build(BuildContext context) {
+    this.context = context;
     Collection collection = ModalRoute.of(context).settings.arguments;
     return Scaffold(
       appBar: AppBar(),
@@ -14,23 +17,43 @@ class CollectionDetail extends StatelessWidget {
           Image.asset(collection.imageUrl),
           Card(
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
                 Text(collection.collectionName,
                   style: TextStyle(
                     color: Colors.red,
                     fontSize: 25,
                     decoration: TextDecoration.underline, 
+                    fontWeight: FontWeight.bold,
                   ),
                 ),
-                Text(collection.description),
+                Text(collection.description,
+                      style: TextStyle(
+                        fontSize: 18,
+                      ),
+                ),
               ],
             ),
           ),
           RaisedButton(
-            child: Text('Users Voice\n<他ユーザからのコメント欄>'),
+            child: Text('　　　　Users Voice　　　　\n（他ユーザからのコメント欄）',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
             color: Colors.blue,
             textColor: Colors.white,
-            onPressed: () {},
+            shape: BeveledRectangleBorder(
+            borderRadius: BorderRadius.circular(10.0),
+            ),
+            onPressed: () {
+              Navigator.pushNamed(
+                this.context, 
+                '/UsrVcLst',
+                arguments: collection
+              );
+            },
           ),
         ],
       ),
