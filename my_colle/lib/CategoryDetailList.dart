7cf459fb49c3d799efce37f24dfbd29f5f55b7da
sc_wakimoto <wakimoto-tsubasa@softcomm.co.jp>
2020-06-24 18:05:53 +0900

    データーベース取得箇所更新

--- my_colle/lib/CategoryDetailList.dart
+++ my_colle/lib/CategoryDetailList.dart
@@ -21,13 +21,15 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 class CategoryDetailList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    var categorytitle =ModalRoute.of(context).settings.arguments;
+    var categorytitle = ModalRoute.of(context).settings.arguments;
     return Scaffold(
       appBar: new AppBar(),
       body: new Container(
           decoration: new BoxDecoration(
             image: new DecorationImage(
-              image: AssetImage('images/background.png'),
+              image: AssetImage('images/top.png'),
+              colorFilter: new ColorFilter.mode(
+                  Colors.black.withOpacity(0.2), BlendMode.dstATop),
               fit: BoxFit.cover,
             ),
           ),
@@ -45,40 +47,9 @@ class CategoryDetailList extends StatelessWidget {
                   ),
                 ),
               ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  Container(
-                    height: 40,
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(
-                      "・$categorytitle・・・・・・・・・15",
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  Container(
-                    height: 30,
-                    //padding: const EdgeInsets.all(8.0),
-
-                    child: Text(
-                      "      　レコード・・・・・・・・・8",
-                      style: TextStyle(
-                        fontSize: 16,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
+              __CategoryDetailListState(),
               Container(
-                height: 480,
+                height: 510,
                 child: _CategoryDetailListState(),
               )
             ],
@@ -88,20 +59,72 @@ class CategoryDetailList extends StatelessWidget {
 }
 
 class _CategoryDetailListState extends StatelessWidget {
+  String subcategory = "";
   @override
   Widget build(BuildContext context) {
-    var categorytitle =ModalRoute.of(context).settings.arguments;
+    var categorytitle = ModalRoute.of(context).settings.arguments;
     return StreamBuilder<QuerySnapshot>(
-      stream: Firestore.instance.collection('myroom').where("category", isEqualTo: categorytitle).snapshots(),
+      stream: Firestore.instance
+          .collection('myroom')
+          .where("category", isEqualTo: categorytitle)
+          // .orderBy("subcategory")
+          .snapshots(),
       builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
         if (snapshot.hasError) return new Text('Error:');
         switch (snapshot.connectionState) {
           case ConnectionState.waiting:
             return new Text('Loading...');
           default:
+          // {
+            // var mappedFruits=snapshot.data.documents.map((DocumentSnapshot document) => document["category"]).toList();
+            // print(mappedFruits);
+          // }
             return new ListView(
               children:
                   snapshot.data.documents.map((DocumentSnapshot document) {
+                // print(document.documentID);
+                {
+                  if (subcategory != document["subcategory"]) {
+                    subcategory = document["subcategory"];
+                    return InkWell(
+                        child: Column(
+                      children: <Widget>[
+                        Container(
+                            height: 35,
+                            child: ListTile(
+                              title: new Text(
+                                  "　${document['subcategory']}・・・・・${snapshot.data.documents.length}",
+                                  style: TextStyle(
+                                    fontSize: 16,
+                                  )),
+                              dense: true,
+                            ),
+                            decoration: BoxDecoration(
+                              color: Colors.black.withOpacity(0.0),
+                            )),
+                        Container(
+                          child: Card(
+                            child: ListTile(
+                              title: new Text(
+                                document['title'],
+                                style: TextStyle(
+                                  color: Colors.red,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              subtitle: new Text(document['body']),
+                              leading: Icon(Icons.person),
+                              onTap: () {
+                                Navigator.pushNamed(context, '/MyRmTop',
+                                    arguments: document['title']);
+                              },
+                            ),
+                          ),
+                        )
+                      ],
+                    ));
+                  }
+                }
                 return InkWell(
                   child: Card(
                     child: ListTile(
@@ -115,14 +138,52 @@ class _CategoryDetailListState extends StatelessWidget {
                       subtitle: new Text(document['body']),
                       leading: Icon(Icons.person),
                       onTap: () {
-                        Navigator.pushNamed(
-                          context, '/MyRmTop',
-                          arguments: document['title']);
+                        Navigator.pushNamed(context, '/MyRmTop',
+                            arguments: document['title']);
                       },
                     ),
                   ),
                 );
-              }).toList(),
+              }).toList()..sort(),
+            );
+        }
+      },
+    );
+  }
+}
+
+class __CategoryDetailListState extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var categorytitle = ModalRoute.of(context).settings.arguments;
+    return StreamBuilder<QuerySnapshot>(
+      stream: Firestore.instance
+          .collection('myroom')
+          .where("category", isEqualTo: categorytitle)
+          .snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) return new Text('Error:');
+        switch (snapshot.connectionState) {
+          case ConnectionState.waiting:
+            return new Text('Loading...');
+          default:
+            return new Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: <Widget>[
+                Container(
+                  height: 40,
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    "・$categorytitle" +
+                        "・・・・・・" +
+                        "${snapshot.data.documents.length.toString()}",
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+              ],
             );
         }
       },
